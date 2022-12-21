434da4d68a04bc41fb86e8db303ea9342e16e138
Pietro Bergamaschi <pietrovb25@gmail.com>
2022-12-20 21:22:27 -0300

    Finaliza componente NoteItem

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -3,6 +3,7 @@ import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
 import { NoteItem } from "../../components/NoteItem";
 import { Section } from "../../components/Section";
+import { Button } from "../../components/Button";
 
 import { Container, Form } from "./styles";
 
@@ -25,6 +26,15 @@ export function New() {
             <NoteItem value="https://rocketseat.com.br" />
             <NoteItem isNew placeholder="Novo link" />
           </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="react" />
+              <NoteItem isNew placeholder="Nova Tag" />
+            </div>
+          </Section>
+
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
